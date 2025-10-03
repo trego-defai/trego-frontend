25abd406fa62c60eaca9e2efb0fcddffc7a08e92
le-dat <lqdat87@gmail.com>
2025-10-03 23:40:40 +0700

    refactor: update logic render wallet

--- src/components/wallet/TregoWallet.tsx
+++ src/components/wallet/TregoWallet.tsx
@@ -3,19 +3,29 @@
 import { Button } from "@/components/ui/button";
 import { getShortAddress } from "@/lib/utils";
 import { walletService } from "@/service/walletService";
+import { useWalletStore } from "@/store/useWalletStore";
 import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 import GenerateWalletButton from "./GenerateWalletButton";
 
 const TregoWallet = () => {
+  const { setAddress } = useWalletStore();
+
   const { data: wallet, isLoading } = useQuery({
     queryKey: ["wallet"],
     queryFn: () => walletService.getWallet(),
   });
 
+  useEffect(() => {
+    if (wallet?.data?.appAddress) {
+      setAddress(wallet?.data?.appAddress || "");
+    }
+  }, [wallet?.data?.appAddress]);
+
   if (isLoading) {
     return (
       <Button variant="primary" size="icon" disabled className="min-w-[200px]">
-        Loading...                  
+        Loading...
       </Button>
     );
   }
@@ -31,4 +41,4 @@ const TregoWallet = () => {
   return <GenerateWalletButton />;
 };
 
-export default TregoWallet; 
+export default TregoWallet;
